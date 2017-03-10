dc37008e960aeaad22a6318a0f8c53580fd3c1dc
Tomas Libal <tomas@libal.eu>
2017-03-10 23:30:48 +0800

    planet_mean_anomaly calculation

--- sunpos/sunpos.py
+++ sunpos/sunpos.py
@@ -1,8 +1,21 @@
 import fire
+from julday import JulianDayNumber
+
+"""
+    Returns the Mean Anomaly of a planet at a given time (Julian Day Number)
+
+    Source: http://aa.quae.nl/en/reken/zonpositie.html
+
+    @param m0 constant
+    @param m1 constant
+    @param jdn julian day number
+"""
+def planet_mean_anomaly(m0, m1, jdn):
+    return (m0 + m1 x (jdn - JulianDayNumber(2000, 1, 1))) % 360
 
 
 class SunposApp():
-    def foo(self):
+    def position(self, year, month, day, lat, lon, planet='Earth'):
         pass
         
         
